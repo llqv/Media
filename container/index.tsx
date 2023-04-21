aaf6e4d70b3a190abdcd0abaf404f331d34df6d9
llqv <lequangvu070501@gmail.com>
2023-04-21 16:52:08 +0700

    first commit

--- container/index.tsx
+++ container/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
+import jsQR from 'jsqr';
 
 const CameraApp = () => {
-  const webcamRef = React.useRef(null);
+  const webcamRef = React.useRef<Webcam>(null);
 
   useEffect(() => {
     const checkUserMedia = async () => {
@@ -13,8 +14,26 @@ const CameraApp = () => {
         alert('Bạn không cho phép truy cập camera');
       }
     };
-
     checkUserMedia();
+
+    const interval = setInterval(() => {
+      if (webcamRef.current && webcamRef.current.video && webcamRef.current.video.readyState === 4) {
+        const video = webcamRef.current.video as HTMLVideoElement;
+        const canvas = document.createElement('canvas');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const ctx = canvas.getContext('2d');
+        if (ctx && canvas) {
+          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+          const code = jsQR(imageData.data, canvas.width, canvas.height);
+          if (code) {
+            console.log('QR Code:', code.data);
+          }
+        }
+      }
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
